4f1f27018399ffe3f0d0ecc1917013d4878106c5
iFyn_ <82264369+i-Fyn@users.noreply.github.com>
2021-11-27 10:38:12 +0800

    Update [抽奖]欢太宠粉计划.js

--- res/task/[抽奖]欢太宠粉计划.js
+++ res/task/[抽奖]欢太宠粉计划.js
@@ -121,7 +121,7 @@ function lottery(aid, lid, name, sku, spu) {
             }
         }
     } else {
-        report("日志", "--------" + name + "抽奖--------");
+        console.log("--------" + name + "抽奖--------");
 
         var i = 0;
         while (i < 10) {
